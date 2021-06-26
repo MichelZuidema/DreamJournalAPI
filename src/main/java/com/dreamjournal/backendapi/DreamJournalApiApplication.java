e6d8876a11c8d66a74bd812ae2b689415f1b1b61
Michel Zuidema <michelgzuidema@gmail.com>
2021-06-26 16:24:00 +0200

    Initial commit

---
+++ src/main/java/com/dreamjournal/backendapi/DreamJournalApiApplication.java
@@ -0,0 +1,13 @@
+package com.dreamjournal.backendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamJournalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamJournalApiApplication.class, args);
+	}
+
+}
